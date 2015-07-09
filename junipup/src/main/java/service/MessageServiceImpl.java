d4f393c803fe6891ac51a27f2be45739fd6d4ef3
user <user@user-PC>
2015-07-09 15:45:34 +0900

    commit

---
+++ junipup/src/main/java/service/MessageServiceImpl.java
@@ -0,0 +1,46 @@
+package service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.MessageDao;
+import model.Messaging;
+@Service
+public class MessageServiceImpl implements MessageService {
+	@Autowired
+	MessageDao md;
+
+	@Override
+	public List<Messaging> listMessageNick(String nickname) {
+		
+		return md.listMessageNick(nickname);
+	}
+
+	@Override
+	public int insertMessage(Messaging message) {
+		
+		return md.insertMessage(message);
+	}
+
+	@Override
+	public Messaging selectMessageNick(int mnum) {
+		
+		return md.selectMessageNick(mnum);
+	}
+
+	@Override
+	public int deleteMessaging(int mnum) {
+		
+		return md.deleteMessaging(mnum);
+	}
+
+	@Override
+	public int upDateMessagingConfirm(int mnum) {
+		
+		return md.upDateMessagingConfirm(mnum);
+	}
+
+}
