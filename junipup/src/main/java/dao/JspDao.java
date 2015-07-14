9658140f36a0357e399f31cb3a629a762a4a065e
402-f3 <402-f3@402-f3-PC>
2015-07-14 10:34:57 +0900

    vc

---
+++ junipup/src/main/java/dao/JspDao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import java.util.List;
+
+import model.JspBoard;
+import model.Member;
+
+public interface JspDao {
+	public int jspInsert(JspBoard jsp);
+	int total();
+	List<JspBoard> listJsp(JspBoard jsp);
+	public JspBoard jspDetail(int bnum);
+	public void jspDelete(int bnum);
+	public void insertBoard();
+	public int jspUpdate(JspBoard jsp);
+	public int totalJspCategory(String category);
+	List<JspBoard> listJspCategory(JspBoard db);
+	void jspPointUp5(Member member);
+	Member selectNickname(String nickname);
+}
